54a85220062e132d13a07dfe6f431b1e56154ac1
DuckTurtle <qschnell04@gmail.com>
2023-04-13 18:48:50 -0500

    help

--- assets/js/secondscript.js
+++ assets/js/secondscript.js
@@ -1,30 +1,64 @@
+var apiKey = "5282121f1a385049aa27e309e97fc347";
+var city = document.querySelector('#cityTypeBox').value;
+var date = dayjs().format("MM/DD/YYYY");
+var runs = 0;    
 
-
-    
-
-
-
-
-async function getinfo(){
-    var resortName = "whistler-blackcomb"
-    //localStorage.getitem(location);
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '957973f9b9msh265c2394e55eefep154fecjsn6252ffa0bc5b',
-		'X-RapidAPI-Host': 'ski-resorts-and-conditions.p.rapidapi.com'
-	}
-};
-
-var dataFetch = fetch('https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort'+ '/' + resortName, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-    let data = await dataFetch;
-    return data;
-};
-async function setElements(){
-    var data = await getinfo();
-    console.log(data);
+async function getOtherDayWeather (){
+    var forcastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
+    let dataResults = fetch(forcastAPI)
+    .then(function(response){
+        var results = response.json();
+        console.log(results);
+            return results;
+        });
+        let data = await dataResults;
+        return data;
 }
-setElements();
+//sets all the text content for curent day weather
+ async function setCurrentDay(){
+    var data = await getWeather(city);
+    console.log(data);
+    // checks for valid data
+    if (!data){
+        console.error("Please input a City");
+        return;
+    }
+    //creates element for location and date
+    var curentDayAnchor = $("#currentDay");
+    var daytext = $("<h3>");
+    daytext.addClass("");
+    daytext.text(data.name + " (" + date + ") ");
+    curentDayAnchor.append(daytext);
+    // adds weather img
+    var coolIcon = $("<img>");
+    coolIcon.attr("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+    curentDayAnchor.append(coolIcon);
+//  adds tempature element at text
+    var tempa = $("<p>");
+    tempa.addClass("");
+    tempa.text("Temp: " + data.main.temp + "°F");
+    curentDayAnchor.append(tempa);
+// above but for wind
+    var windy = $("<p>");
+    windy.addClass("");
+    windy.text("Wind: " + data.wind.speed + " MPH");
+    curentDayAnchor.append(windy);
+// above also but for humidaty
+    var water = $("<p>");
+    water.addClass("");
+    water.text("Humidity: " + data.main.humidity + "%");
+    curentDayAnchor.append(water);
+
+};
+function clearOldStuff(){
+    var curentday = document.getElementById("currentDay");
+    console.log(curentday);
+    while (curentday.firstChild) {
+        curentday.removeChild(curentday.firstChild);
+      }
+    
+    var c5day = document.getElementById("5dayforcast");
+    while (c5day.firstChild) {
+        c5day.removeChild(c5day.firstChild);
+      }
+};
