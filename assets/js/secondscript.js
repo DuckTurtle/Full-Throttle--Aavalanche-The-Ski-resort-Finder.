710296ad1fd273c991a63efa3df24df80da7dc42
DuckTurtle <qschnell04@gmail.com>
2023-04-13 21:20:00 -0500

    built out api call and element creation

--- assets/js/secondscript.js
+++ assets/js/secondscript.js
@@ -1,10 +1,89 @@
 var apiKey = "W1L4ukvhh9ASpC8FYICufwmnwxcv6i16sNbSq9ZY";
+var curentPark = $("#itemStorgeBlock");
 //var city = document.querySelector('#cityTypeBox').value;
 //var date = dayjs().format("MM/DD/YYYY");
 var runs = 0;    
+var parkCode = "apco";
+var city = setCity();
 
+async function setCity(){
+    var data = await getParkInfo(parkCode);
+    console.log(data);
+    // checks for valid data
+    if (!data){
+        console.error("Car Crash");
+        return;
+    }
+    var currentCity = data.data[0].addresses[0].city
+    var ccity = currentCity;
+    return ccity;
+
+};
+async function getCity(){
+    var forcastAPI = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkCode + "&api_key=" + apiKey;
+    let dataResults = fetch(forcastAPI)
+    .then(function(response){
+        var results = response.json();
+        console.log(results);
+            return results;
+        });
+        let data = await dataResults;
+        return data;
+}
+async function getParkInfo (){
+    var forcastAPI = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkCode + "&api_key=" + apiKey;
+    let dataResults = fetch(forcastAPI)
+    .then(function(response){
+        var results = response.json();
+        console.log(results);
+            return results;
+        });
+        let data = await dataResults;
+        return data;
+}
+async function setParkInfo(){
+    var data = await getParkInfo(parkCode);
+    console.log(data);
+    // checks for valid data
+    if (!data){
+        console.error("Please input a City");
+        return;
+    }
+    var parkName = $("<h2>");
+    parkName.text(data.data[0].fullName);
+    curentPark.append(parkName);
+    var parkcost = $("<p>");
+    parkcost.text(data.data[0].entranceFees[0].description);
+    curentPark.append(parkcost);
+    var parkCall = $("<p>");
+    parkCall.text(data.data[0].contacts.phoneNumbers[0].phoneNumber);
+    curentPark.append(parkCall);
+    var parklocation = $("<p>");
+    parklocation.text(data.data[0].directionsInfo);
+    curentPark.append(parklocation);
+    var parkWebsite = $("<p>");
+    parkWebsite.text(data.data[0].directionsUrl);
+    curentPark.append(parkWebsite);
+    var parkHours = $("<p>");
+    parkHours.text(data.data[0].operatingHours[0].description);
+    curentPark.append(parkHours);
+
+}
+//calls the weather api with given city.
+async function getWeather (){
+    var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+    let dataResults = fetch(weatherAPI)
+    .then(function(response){
+        var results = response.json();
+        console.log(results);
+            return results;
+        });
+        let data = await dataResults;
+        return data;
+}
+//calls the forcast and waits to pass it on till the api responds.
 async function getOtherDayWeather (){
-    var forcastAPI = "https://developer.nps.gov/api/v1/parks?" + "&api_key=" + apiKey;
+    var forcastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
     let dataResults = fetch(forcastAPI)
     .then(function(response){
         var results = response.json();
@@ -50,6 +129,46 @@ async function getOtherDayWeather (){
     curentDayAnchor.append(water);
 
 };
+// sets text values for forcast blocks. does same stuff as the one above but for the forcast
+async function otherDayForcast(){
+    var data = await getOtherDayWeather(city);
+    console.log(data);
+    if (!data){
+        console.error("Please input a City");
+        return;
+    }
+    var forcatDayAnchor = $("#5dayforcast");
+    for(i=4; i<40; i+=8){
+    var blockbox = $("<div>");
+    blockbox.addClass("weatherBlock");
+    forcatDayAnchor.append(blockbox)
+
+    var daytext = $("<h4>");
+    daytext.addClass("");
+    var dateCovert = dayjs(data.list[i].dt_txt).format("MM/DD/YYYY")
+    daytext.text(data.city.name + " (" + dateCovert + ") ");
+    blockbox.append(daytext);
+
+    var coolIcon = $("<img>");
+    coolIcon.attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png");
+    blockbox.append(coolIcon);
+
+    var tempa = $("<p>");
+    tempa.addClass("");
+    tempa.text("Temp: " + data.list[i].main.temp + "°F");
+    blockbox.append(tempa);
+
+    var windy = $("<p>");
+    windy.addClass("");
+    windy.text("Wind: " + data.list[i].wind.speed + " MPH");
+    blockbox.append(windy);
+
+    var water = $("<p>");
+    water.addClass("");
+    water.text("Humidity: " + data.list[i].main.humidity + "%");
+    blockbox.append(water);
+    }
+};
 function clearOldStuff(){
     var curentday = document.getElementById("currentDay");
     console.log(curentday);
@@ -62,4 +181,5 @@ function clearOldStuff(){
         c5day.removeChild(c5day.firstChild);
       }
 };
-getOtherDayWeather();
+getParkInfo();
+setParkInfo();
