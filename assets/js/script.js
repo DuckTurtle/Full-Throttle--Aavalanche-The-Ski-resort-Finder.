e0f33b6c06fc72ecaf60138cc0c152a758bb41ca
DuckTurtle <qschnell04@gmail.com>
2023-04-17 20:18:54 -0500

    added api call with park option build out

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,33 @@ window.onclick = function(event) {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pass park code  though location
+async function getCity(){
+    var state = "wi";
+    var parkAPI = "https://developer.nps.gov/api/v1/parks?stateCode=" + state + "&api_key=W1L4ukvhh9ASpC8FYICufwmnwxcv6i16sNbSq9ZY";
+    let dataResults = fetch(parkAPI)
+    .then(function(response){
+        var results = response.json();
+        console.log(results);
+            return results;
+        });
+        let data = await dataResults;
+        return data;
+}
+getCity();
