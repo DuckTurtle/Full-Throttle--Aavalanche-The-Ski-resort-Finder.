b2a085cc60abee5129488eb389a0ec2e978d139c
makgeolli <mordredphantom@gmail.com>
2023-04-19 18:19:31 -0500

    Fixed popup for 'parkdiv' variables on hover, as well as added animations for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,7 +69,7 @@ async function setParkBubbles(){
     var parkdiv = $("<button>");
     parkdiv.attr({id: data.data[i].parkCode});
     pageAnchor.append(parkdiv);
-    parkdiv.addClass("parkBlock                                                                    ");
+    parkdiv.addClass("parkBlock");
 
     var coolIcon = $("<img>");
     coolIcon.attr("src",  data.data[i].images[0].url);
@@ -80,29 +80,8 @@ async function setParkBubbles(){
     parkName.addClass("text-center text-2xl p-2");
     parkName.text(data.data[i].fullName);
     parkdiv.append(parkName);
-    /* not currently working
-    // Creates popup box
-    parkdiv.on("mouseover", function (event) {
-        var popup = $("<div>");
-        popup.addClass("popup");
-        popup.text(data.data[i].exceptionhours);
-
-        // Positions popup box
-        var parkdivPos = parkdiv.offset();
-        popup.css({
-          top: parkdivPos.top + parkdiv.outerHeight() + 10,
-          left: parkdivPos.left,
-        });
-
-        // Displays the popup on page
-        $("body").append(popup);
-      });
-
-      parkdiv.on("mouseout", function (event) {
-        // Remove the popup when the mouse leaves the parkdiv element
-        $(".popup").remove();
-      });
-*/
+    
+ 
     parkdiv.on("click", function (event) {
         event.preventDefault();
 
@@ -112,7 +91,20 @@ async function setParkBubbles(){
         location.assign(queryString);
       });
 
-
+    // Add mouseover event listener to display additional information
+    parkdiv.on("mouseover", function () {
+        var parkInfo = $("<div>");
+        parkInfo.addClass("parkBlock-info");
+        parkInfo.text("This park is located in " + state + "! Click me to show more info!");
+  
+        // Append the information box to the parent element
+        $(this).append(parkInfo);
+      });
+  
+      // Add mouseout event listener to remove the information box
+      parkdiv.on("mouseout", function () {
+        $(this).find(".parkBlock-info").remove();
+      });
     }
      
 };
